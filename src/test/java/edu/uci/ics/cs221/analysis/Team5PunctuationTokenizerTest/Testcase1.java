106fa9a036e396eff6450baf35e56fb4308ced47
chenghz4 <chenghz4@uci.edu>
2019-04-06 15:44:45 -0700

    test

--- src/test/java/edu/uci/ics/cs221/analysis/Team5PunctuationTokenizerTest/Testcase1.java
+++ src/test/java/edu/uci/ics/cs221/analysis/Team5PunctuationTokenizerTest/Testcase1.java
@@ -1,4 +1,15 @@
 package edu.uci.ics.cs221.analysis.Team5PunctuationTokenizerTest;
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
 public class Testcase1 {
+
+    String message = "Robert";
+
+
+    @Test
+    public void testPrintMessage() {
+        System.out.println("Inside testPrintMessage()");
+        assertEquals(message, "");
+    }
 }
--- src/test/java/edu/uci/ics/cs221/analysis/Team5PunctuationTokenizerTest/Testcase2.java
+++ src/test/java/edu/uci/ics/cs221/analysis/Team5PunctuationTokenizerTest/Testcase2.java
@@ -1,4 +1,12 @@
 package edu.uci.ics.cs221.analysis.Team5PunctuationTokenizerTest;
-
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
 public class Testcase2 {
+
+    String message = "Robert";
+    @Test
+    public void testPrintMessage() {
+        System.out.println("Inside testPrintMessage()");
+        assertEquals(message, "");
+    }
 }
