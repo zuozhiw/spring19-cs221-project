36a3159bdd35c9933db468bb7fc51d8f708d4c6f
FuyaoLi2017 <1605419584@qq.com>
2019-04-07 16:31:52 -0700

    update mistakes

--- src/test/java/edu/uci/ics/cs221/analysis/punctuation/Team2PunctuationTokenizerTest.java
+++ src/test/java/edu/uci/ics/cs221/analysis/punctuation/Team2PunctuationTokenizerTest.java
@@ -17,7 +17,7 @@ public class Team2PunctuationTokenizerTest {
     @Test
     public void test1() {
         String text = "UCI: \n \n a public research university located in Irvine, \t \t California!";
-        List<String> expected = Arrays.asList("uci", "public", "research", "university", "located","irvine","california");
+        List<String> expected = Arrays.asList("uci:", "public", "research", "university", "located","irvine","california");
         PunctuationTokenizer tokenizer = new PunctuationTokenizer();
 
         assertEquals("check whether multiple newlines and tabs works",expected, tokenizer.tokenize(text));
@@ -30,7 +30,7 @@ public class Team2PunctuationTokenizerTest {
     @Test
     public void test2() {
         String text = "\uD83D\uDC34 is a very cute horse head!";
-        List<String> expected = Arrays.asList("\uD83D\uDC34", "very", "cute", "horse", "head");
+        List<String> expected = Arrays.asList("\uD83D\uDC34", "cute", "horse", "head");
         PunctuationTokenizer tokenizer = new PunctuationTokenizer();
 
         assertEquals("check whether the tokenizer can deal with emojis" ,expected, tokenizer.tokenize(text));
@@ -42,7 +42,7 @@ public class Team2PunctuationTokenizerTest {
     @Test
     public void test3() {
         String text = "UCI : \na, public research university located in Irvine,California!!!!";
-        List<String> expected = Arrays.asList("uci", "public", "research", "university", "located","irvine","california");
+        List<String> expected = Arrays.asList("uci", ":", "public", "research", "university", "located","irvine","california");
         PunctuationTokenizer tokenizer = new PunctuationTokenizer();
 
         assertEquals("check consecutive punctuation works",expected, tokenizer.tokenize(text));
