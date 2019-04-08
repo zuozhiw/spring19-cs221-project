405d71b6c48349347c2c0d660a8ebf681c29509c
zor077 <45053898+zor077@users.noreply.github.com>
2019-04-07 22:28:34 -0700

    Update PorterStemmer.java

--- src/main/java/edu/uci/ics/cs221/analysis/PorterStemmer.java
+++ src/main/java/edu/uci/ics/cs221/analysis/PorterStemmer.java
@@ -1,13 +1,12 @@
 package edu.uci.ics.cs221.analysis;
 
 /**
- * Project 1, task 3: Incorporate a porter stemmer.
+ * Project 1, task 3: Incorporate a Porter stemmer.
  *
  * Stemming is the process of reducing a word into its "stem" ("root") form.
  *
  * Porter stemming is a classic and popular algorithm that uses a set of rules and steps to process a token.
- * Implementing porter stemmer is not technically interesting - it just consists a bunch of if-else statements,
- * therefore we ask you to simply incorporate an existing porter stemmer implementation into this project.
+ * We ask you to incorporate the following existing Porter stemmer implementation into this project.
  *
  * https://github.com/apache/lucene-solr/blob/master/lucene/analysis/common/src/java/org/apache/lucene/analysis/en/PorterStemmer.java
  *
