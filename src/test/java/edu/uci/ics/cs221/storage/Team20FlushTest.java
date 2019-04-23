44f752bcf762de4c0b7f0c010cfec8f6af8b64a4
Pritha Dawn <prithadawn@dhcp-v099-006.mobile.uci.edu>
2019-04-22 22:24:55 -0700

    added after class method to delete files created during testing

--- src/test/java/edu/uci/ics/cs221/storage/Team20FlushTest.java
+++ src/test/java/edu/uci/ics/cs221/storage/Team20FlushTest.java
@@ -3,12 +3,16 @@ package edu.uci.ics.cs221.storage;
 import edu.uci.ics.cs221.analysis.NaiveAnalyzer;
 import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
 import edu.uci.ics.cs221.index.inverted.InvertedIndexSegmentForTest;
+import org.junit.AfterClass;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.io.File;
+
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,7 +31,7 @@ public class Team20FlushTest {
         expectedDocuments.put(0, d1);
         expectedDocuments.put(1, d2);
         NaiveAnalyzer analyzer = new NaiveAnalyzer();
-        InvertedIndexManager ii = InvertedIndexManager.createOrOpen(" ", analyzer) ;
+        InvertedIndexManager ii = InvertedIndexManager.createOrOpen("index/Team20FlushTest/", analyzer) ;
         ii.DEFAULT_FLUSH_THRESHOLD = 2;
         ii.addDocument(d1);
         ii.addDocument(d2);
@@ -47,11 +51,80 @@ public class Team20FlushTest {
         Map<Integer, Document> expectedDocuments = new HashMap<>();
 
         NaiveAnalyzer analyzer = new NaiveAnalyzer();
-        InvertedIndexManager ii = InvertedIndexManager.createOrOpen(" ", analyzer) ;
+        InvertedIndexManager ii = InvertedIndexManager.createOrOpen("index/Team20FlushTest/", analyzer) ;
 
         ii.flush();
         assertEquals(0, ii.getNumSegments());
 
     }
 
-}
+    @AfterClass
+    public static void deleteFiles() {
+               String SRC_FOLDER = "index/Team20FlushTest/";
+
+                File directory = new File(SRC_FOLDER);
+
+                //make sure directory exists
+                if(!directory.exists()){
+
+                    System.out.println("Directory does not exist.");
+                    System.exit(0);
+
+                }else{
+
+                    try{
+
+                        delete(directory);
+
+                    }catch(IOException e){
+                        e.printStackTrace();
+                        System.exit(0);
+                    }
+                }
+
+                System.out.println("Done");
+            }
+
+            public static void delete(File file)
+                    throws IOException {
+
+                if(file.isDirectory()){
+
+                    //directory is empty, then delete it
+                    if(file.list().length==0){
+
+                        file.delete();
+                        System.out.println("Directory is deleted : "
+                                + file.getAbsolutePath());
+
+                    }else{
+
+                        //list all the directory contents
+                        String files[] = file.list();
+
+                        for (String temp : files) {
+                            //construct the file structure
+                            File fileDelete = new File(file, temp);
+
+                            //recursive delete
+                            delete(fileDelete);
+                        }
+
+                        //check the directory again, if empty then delete it
+                        if(file.list().length==0){
+                            file.delete();
+                            System.out.println("Directory is deleted : "
+                                    + file.getAbsolutePath());
+                        }
+                    }
+
+                }else{
+                    //if file, then delete it
+                    file.delete();
+                    System.out.println("File is deleted : " + file.getAbsolutePath());
+                }
+            }
+        }
+
+
+
