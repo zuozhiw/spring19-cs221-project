a432c8e3a3d3482929b9ac759f2fe10241cc39ec
KaifuJ <jkf0128@gmail.com>
2019-04-23 21:36:19 -0700

    modify tests

--- src/test/java/edu/uci/ics/cs221/index/inverted/Team10MergeSegmentTest.java
+++ src/test/java/edu/uci/ics/cs221/index/inverted/Team10MergeSegmentTest.java
@@ -15,26 +15,25 @@ import static org.junit.Assert.assertEquals;
 
 public class Team10MergeSegmentTest {
 
-    private String path = "./index/team10test";
+    private String path = "./index/Team10MergeSearchTest";
 
     @Test
     public void test1() {
         InvertedIndexManager invertedmanager;
         invertedmanager = InvertedIndexManager.createOrOpen(path, new ComposableAnalyzer(new PunctuationTokenizer(),new PorterStemmer()));
 
-        invertedmanager.DEFAULT_MERGE_THRESHOLD = 4;
         Document doc0 = new Document("what is he doing today");
         Document doc1 = new Document("what a cute dog");
         Document doc2 = new Document("i saw you doing there today");
         Document doc3 = new Document("a dog is there");
         invertedmanager.addDocument(doc0);
         invertedmanager.addDocument(doc1);
-        invertedmanager.flush();
+        invertedmanager.flush(); // segment 0
         invertedmanager.addDocument(doc2);
         invertedmanager.addDocument(doc3);
-        invertedmanager.flush();
+        invertedmanager.flush(); // segment 1
 
-        Map<String,List<Integer>> expectedlist = new HashMap<String, List<Integer>>();
+        Map<String,List<Integer>> expectedlist = new HashMap<>();
         expectedlist.put("what",Arrays.asList(0,1));
         expectedlist.put("is",Arrays.asList(0,3));
         expectedlist.put("he",Arrays.asList(0));
@@ -47,7 +46,7 @@ public class Team10MergeSegmentTest {
         expectedlist.put("saw",Arrays.asList(2));
         expectedlist.put("you",Arrays.asList(2));
         expectedlist.put("there",Arrays.asList(2,3));
-        Map<Integer, Document> expecteddocuments = new HashMap<Integer, Document>();
+        Map<Integer, Document> expecteddocuments = new HashMap<>();
         expecteddocuments.put(0,doc0);
         expecteddocuments.put(1,doc1);
         expecteddocuments.put(2,doc2);
@@ -56,7 +55,6 @@ public class Team10MergeSegmentTest {
         InvertedIndexSegmentForTest expected = new InvertedIndexSegmentForTest(expectedlist,expecteddocuments);
         invertedmanager.mergeAllSegments();
         assertEquals(expected,invertedmanager.getIndexSegment(0));
-        invertedmanager.DEFAULT_MERGE_THRESHOLD = 8;
     }
 
     @Test
@@ -74,33 +72,33 @@ public class Team10MergeSegmentTest {
         Document doc7 = new Document("cat pig dog");
         invertedmanager.addDocument(doc0);
         invertedmanager.addDocument(doc1);
-        invertedmanager.flush();
+        invertedmanager.flush(); // segment 0
         invertedmanager.addDocument(doc2);
         invertedmanager.addDocument(doc3);
-        invertedmanager.flush();
+        invertedmanager.flush(); // segment 1
         invertedmanager.addDocument(doc4);
         invertedmanager.addDocument(doc5);
-        invertedmanager.flush();
+        invertedmanager.flush(); // segment 2
         invertedmanager.addDocument(doc6);
         invertedmanager.addDocument(doc7);
-        invertedmanager.flush();
+        invertedmanager.flush(); // segment 3
 
-        Map<String,List<Integer>> expectedlist1 = new HashMap<String, List<Integer>>();
+        Map<String,List<Integer>> expectedlist1 = new HashMap<>();
         expectedlist1.put("cat",Arrays.asList(0,1,3));
         expectedlist1.put("dog",Arrays.asList(0,1,2));
         expectedlist1.put("wolf",Arrays.asList(1,2,3));
-        Map<Integer, Document> expecteddocuments1 = new HashMap<Integer, Document>();
+        Map<Integer, Document> expecteddocuments1 = new HashMap<>();
         expecteddocuments1.put(0,doc0);
         expecteddocuments1.put(1,doc1);
         expecteddocuments1.put(2,doc2);
         expecteddocuments1.put(3,doc3);
 
-        Map<String,List<Integer>> expectedlist2 = new HashMap<String, List<Integer>>();
+        Map<String,List<Integer>> expectedlist2 = new HashMap<>();
         expectedlist2.put("pig", Arrays.asList(0,1,3));
         expectedlist2.put("wolf",Arrays.asList(0,1));
         expectedlist2.put("cat",Arrays.asList(0,2,3));
         expectedlist2.put("dog",Arrays.asList(1,3));
-        Map<Integer, Document> expecteddocuments2 = new HashMap<Integer, Document>();
+        Map<Integer, Document> expecteddocuments2 = new HashMap<>();
         expecteddocuments2.put(0,doc4);
         expecteddocuments2.put(1,doc5);
         expecteddocuments2.put(2,doc6);
@@ -114,7 +112,7 @@ public class Team10MergeSegmentTest {
     }
 
     @After
-    public void clearing(){
+    public void clean(){
         File file = new File(path);
         String[] filelist = file.list();
         for(String f : filelist){
