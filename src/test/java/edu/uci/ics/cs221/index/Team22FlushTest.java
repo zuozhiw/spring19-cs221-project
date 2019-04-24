68d7b9c4794f2b8364aabc0f8a0413f7fbb03330
adsl_chen <adsl_chen@outlook.com>
2019-04-23 22:20:59 -0700

    add comments and merge

--- src/test/java/edu/uci/ics/cs221/index/Team22FlushTest.java
+++ src/test/java/edu/uci/ics/cs221/index/Team22FlushTest.java
@@ -2,6 +2,7 @@ package edu.uci.ics.cs221.index;
 import edu.uci.ics.cs221.analysis.*;
 import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
 import edu.uci.ics.cs221.index.inverted.InvertedIndexSegmentForTest;
+import edu.uci.ics.cs221.index.inverted.PageFileChannel;
 import edu.uci.ics.cs221.storage.Document;
 import org.junit.After;
 import org.junit.Before;
@@ -39,8 +40,8 @@ public class Team22FlushTest {
 
         try{
             File index = new File(folder);
-            String[]entries = index.list();
-            for(String s: entries){
+            String[] f = index.list();
+            for(String s: f){
                 File currentFile = new File(index.getPath(),s);
                 currentFile.delete();
             }
@@ -51,6 +52,14 @@ public class Team22FlushTest {
         Files.deleteIfExists(Paths.get(folder));
 
     }
+
+
+    /*
+    This test case check whether flushed segment stored correctly
+    We check for both 1. invertedIndexList 2. documents store
+
+
+     */
     @Test
     public void verifyContextTest() throws Exception{
         // In this test, we verify the inverted index list in the segment in disk.
@@ -112,18 +121,22 @@ public class Team22FlushTest {
 
     @Test
     public void verifyContextTest2() throws Exception{
+        // store the original threshold
         int old_threshold = InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD;
+
+
         Tokenizer tokenizer = new PunctuationTokenizer();
         Stemmer stemmer = new PorterStemmer();
         analyzer = new ComposableAnalyzer(tokenizer, stemmer); // create composable analyzer for documents tokenization
 
         indexManager = InvertedIndexManager.createOrOpen(folder,analyzer);
 
-        int flush_threshold = 50;
-        int total_docs = 200;
+        int flush_threshold = 50; // our flush threshold
+        int total_docs = 200; // total documents we gonna add
 
         InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = flush_threshold;
 
+        // the documents are created in the combination of these vocabularies
         ArrayList<String> vocabs = new ArrayList<>();
         vocabs.add("apple");
         vocabs.add("banana");
@@ -138,6 +151,8 @@ public class Team22FlushTest {
         }
 
 
+        // generate random documents and add to indexManager
+
         for (int i = 0; i < total_docs; i++){
 
             String[] words = new String [10]; // each document has 10 words
@@ -147,6 +162,7 @@ public class Team22FlushTest {
                 Random random = new Random();
                 words[j] = vocabs.get(random.nextInt(vocabs.size()));
             }
+
             String text = String.join(" ",words);
             Document d = new Document(text);
             indexManager.addDocument(d);
@@ -168,6 +184,112 @@ public class Team22FlushTest {
 
     }
 
+    /* This test checks two aspects of the flush function.
+       1. Check if flush does nothing if no document added
+       2. Create an empty document, and check if flush added the document properly, with empty posting list.
+    */
+    @Test
+    public void checkEmptyTest(){
+        Tokenizer tokenizer = new WordBreakTokenizer();
+        Stemmer stemmer = new PorterStemmer();
+        analyzer = new ComposableAnalyzer(tokenizer, stemmer);
+        indexManager = InvertedIndexManager.createOrOpen(folder,analyzer);
+
+        // check if flush does nothing when no docs added
+        indexManager.flush();
+        assertEquals(indexManager.getNumSegments(), 0);
+
+        // check if empty string document is added properly, but with empty inverted index
+        Document doc1 = new Document("");
+        ArrayList<Document> docs = new ArrayList<>();
+        docs.add(doc1);
+        indexManager.addDocument(doc1);
+        indexManager.flush();
+        assertEquals(indexManager.getNumSegments(), 1);
+        checkSegment(0, docs);
+    }
+
+    // This testcase tests if the system automatically flush when reaching the threshold
+    @Test
+    public void checkThresoldFlushTest(){
+        Tokenizer tokenizer = new PunctuationTokenizer();
+        Stemmer stemmer = new PorterStemmer();
+        analyzer = new ComposableAnalyzer(tokenizer, stemmer); // create composable analyzer for documents tokenization
+        indexManager = InvertedIndexManager.createOrOpen(folder,analyzer);
+
+        // reset the default_flush_threshold
+        int old_threshold = indexManager.DEFAULT_FLUSH_THRESHOLD;
+        indexManager.DEFAULT_FLUSH_THRESHOLD = 5;
+        int new_threshold = indexManager.DEFAULT_FLUSH_THRESHOLD;
+        int numDocs = 23;
+        ArrayList<Document> docs = new ArrayList<>(); // ground true docs
+        for (int i = 0; i < numDocs; i++){
+            Document doc = new Document(String.valueOf(i));
+            indexManager.addDocument(doc);
+            docs.add(doc);
+            // reset ground true docs when reach threshold
+            if (i % new_threshold == new_threshold - 1){
+                checkSegment(indexManager.getNumSegments()-1, docs);
+                docs = new ArrayList<>();
+            }
+        }
+        assertEquals(indexManager.getNumSegments(), numDocs/new_threshold);
+        indexManager.DEFAULT_FLUSH_THRESHOLD = old_threshold; // change back to original threshold
+    }
+
+
+    // check if read/writer counter updated properly
+    // we check whether the pages take the space at least the length of the keywords
+
+    @Test
+    public void checkPageIOTest(){
+        Tokenizer tokenizer = new PunctuationTokenizer();
+        Stemmer stemmer = new PorterStemmer();
+        analyzer = new ComposableAnalyzer(tokenizer, stemmer); // create composable analyzer for documents tokenization
+        indexManager = InvertedIndexManager.createOrOpen(folder,analyzer);
+
+        // create documents with 4096 characters
+        char[] chars = new char[PageFileChannel.PAGE_SIZE];
+        Arrays.fill(chars, 'a');
+        String text = new String(chars);
+        Document doc1 = new Document(text);
+
+        chars = new char[PageFileChannel.PAGE_SIZE];
+        Arrays.fill(chars, 'b');
+        text = new String(chars);
+        Document doc2 = new Document(text);
+
+        chars = new char[PageFileChannel.PAGE_SIZE];
+        Arrays.fill(chars, 'c');
+        text = new String(chars);
+        Document doc3 = new Document(text);
+
+        // set up ground true documents
+        ArrayList<Document> docs = new ArrayList<>();
+        docs.add(doc1);
+        docs.add(doc2);
+        docs.add(doc3);
+
+        // add documents
+        indexManager.addDocument(doc1);
+        indexManager.addDocument(doc2);
+        indexManager.addDocument(doc3);
+        indexManager.flush();
+
+        // check segment number and write counter
+        assertEquals(indexManager.getNumSegments(), 1);
+        assertTrue(PageFileChannel.writeCounter >= 3);
+
+        // check segment context and read counter
+        checkSegment(0, docs);
+        assertTrue(PageFileChannel.readCounter >= 3);
+    }
+
+
+
+    /*
+    This function create the ground truth of "one segments" of given documents lists
+     */
 
     private HashMap<String, HashSet<Document>> createInvertedIndex2Doc(List<Document> docs){
 
@@ -189,6 +311,7 @@ public class Team22FlushTest {
 
         return output;
     }
+
     // function to check if segment is read correctly
     private void checkSegment(int segmentNum, List<Document> docs){
         // get corresponding segment
