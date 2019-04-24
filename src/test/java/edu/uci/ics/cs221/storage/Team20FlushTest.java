2d8c3f51f557ae626f8d3c88bb09b8af48410d9a
nevedharavi <nevedha.ravi@gmail.com>
2019-04-23 19:01:21 -0700

    modified test cases for flush

--- src/test/java/edu/uci/ics/cs221/storage/Team20FlushTest.java
+++ src/test/java/edu/uci/ics/cs221/storage/Team20FlushTest.java
@@ -18,45 +18,93 @@ import static org.junit.Assert.assertEquals;
 
 public class Team20FlushTest {
 
+    /* Change the flush threshold and see if flush() is automatically called when the threshold is reached.
+    * In this test, we set the threshold to 2. We then add two documents, after which flush() should be
+    * automatically called*/
     @Test
-    public void test1() {
-        /* change the flush threshold and see if flush() is automatically called*/
+    public void check_auto_flush_call() {
         Document d1 = new Document("cat dog");
         Document d2 = new Document("cat elephant");
         Map<String, List<Integer>> expectedPostingList = new HashMap<>();
         expectedPostingList.put("cat", Arrays.asList(0, 1));
         expectedPostingList.put("dog", Arrays.asList(0)) ;
         expectedPostingList.put("elephant", Arrays.asList(1)) ;
-        Map<Integer, Document> expectedDocuments = new HashMap<>();
-        expectedDocuments.put(0, d1);
-        expectedDocuments.put(1, d2);
+        Map<Integer, Document> expectedDocStore = new HashMap<>();
+        expectedDocStore.put(0, d1);
+        expectedDocStore.put(1, d2);
         NaiveAnalyzer analyzer = new NaiveAnalyzer();
         InvertedIndexManager ii = InvertedIndexManager.createOrOpen("index/Team20FlushTest/", analyzer) ;
+
         ii.DEFAULT_FLUSH_THRESHOLD = 2;
         ii.addDocument(d1);
         ii.addDocument(d2);
-        //ii.flush();
+
         assertEquals(1, ii.getNumSegments());
         InvertedIndexSegmentForTest segment;
         segment = ii.getIndexSegment(0);
         assertEquals(expectedPostingList, segment.getInvertedLists());
-        assertEquals(expectedDocuments, segment.getDocuments());
+        assertEquals(expectedDocStore, segment.getDocuments());
     }
 
+    /* Change the flush threshold and see if flush() is automatically called twice when the threshold is reached.
+     * In this test, we set the threshold to 2. We then add four documents. flush() should be
+     * automatically called twice*/
     @Test
-    public void test2() {
-        /* forcefully call flush(), keeping an empty buffer, flush should do nothing*/
+    public void check_auto_flush_call_twice() {
+        Document d1 = new Document("cat dog");
+        Document d2 = new Document("cat elephant");
+        Document d3 = new Document("call car cast");
+        Document d4 = new Document("call canon cast");
 
-        Map<String, List<Integer>> expectedPostingList = new HashMap<>();
-        Map<Integer, Document> expectedDocuments = new HashMap<>();
+        Map<String, List<Integer>> expectedPostingList1 = new HashMap<>();
+        expectedPostingList1.put("cat", Arrays.asList(0, 1));
+        expectedPostingList1.put("dog", Arrays.asList(0)) ;
+        expectedPostingList1.put("elephant", Arrays.asList(1)) ;
+        Map<Integer, Document> expectedDocStore1 = new HashMap<>();
+        expectedDocStore1.put(0, d1);
+        expectedDocStore1.put(1, d2);
 
+        Map<String, List<Integer>> expectedPostingList2 = new HashMap<>();
+        expectedPostingList2.put("call", Arrays.asList(0, 1));
+        expectedPostingList2.put("canon", Arrays.asList(1)) ;
+        expectedPostingList2.put("car", Arrays.asList(0)) ;
+        expectedPostingList2.put("cast", Arrays.asList(0, 1)) ;
+        Map<Integer, Document> expectedDocStore2 = new HashMap<>();
+        expectedDocStore2.put(0, d3);
+        expectedDocStore2.put(1, d4);
+
+        NaiveAnalyzer analyzer = new NaiveAnalyzer();
+        InvertedIndexManager ii = InvertedIndexManager.createOrOpen("index/Team20FlushTest/", analyzer) ;
+
+        ii.DEFAULT_FLUSH_THRESHOLD = 2;
+        ii.addDocument(d1);
+        ii.addDocument(d2);
+        ii.addDocument(d3);
+        ii.addDocument(d4);
+
+        assertEquals(2, ii.getNumSegments());
+
+        InvertedIndexSegmentForTest segment0;
+        segment0 = ii.getIndexSegment(0);
+        assertEquals(expectedPostingList1, segment0.getInvertedLists());
+        assertEquals(expectedDocStore1, segment0.getDocuments());
+
+        InvertedIndexSegmentForTest segment1;
+        segment1 = ii.getIndexSegment(1);
+        assertEquals(expectedPostingList2, segment1.getInvertedLists());
+        assertEquals(expectedDocStore2, segment1.getDocuments());
+    }
+
+    /* Forcefully call flush(), keeping an empty in-memory buffer, flush() should do nothing*/
+    @Test
+    public void check_empty_mem_buffer() {
         NaiveAnalyzer analyzer = new NaiveAnalyzer();
         InvertedIndexManager ii = InvertedIndexManager.createOrOpen("index/Team20FlushTest/", analyzer) ;
 
         ii.flush();
         assertEquals(0, ii.getNumSegments());
-
     }
+
     @Test
     public void test3(){
 
