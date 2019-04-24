f285e4536a02694855cfd2d29c56212bb8ea9c57
Ned <46288336+Nedhoven@users.noreply.github.com>
2019-04-23 21:11:29 -0700

    Update Team19FlushTest.java

--- src/test/java/edu/uci/ics/cs221/index/inverted/Team19FlushTest.java
+++ src/test/java/edu/uci/ics/cs221/index/inverted/Team19FlushTest.java
@@ -1,8 +1,11 @@
-package edu.uci.ics.cs221.index.inverted;
+
+package edu.uci.ics.cs221.index;
 
 import edu.uci.ics.cs221.analysis.ComposableAnalyzer;
 import edu.uci.ics.cs221.analysis.PorterStemmer;
 import edu.uci.ics.cs221.analysis.PunctuationTokenizer;
+import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
+import edu.uci.ics.cs221.index.inverted.InvertedIndexSegmentForTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,14 +18,15 @@ import java.util.*;
 import edu.uci.ics.cs221.analysis.Analyzer;
 import edu.uci.ics.cs221.storage.Document;
 
-public class Team19FlushTest {
-    Analyzer anal = new ComposableAnalyzer(new PunctuationTokenizer(), new PorterStemmer());
+public class FlushTest {
+
+    Analyzer an = new ComposableAnalyzer(new PunctuationTokenizer(), new PorterStemmer());
     InvertedIndexManager iim;
     String file = "./index";
 
     @Before
     public void setup() throws Exception {
-        iim = iim.createOrOpen(file, anal);
+        iim = iim.createOrOpen(file, an);
         iim.DEFAULT_FLUSH_THRESHOLD = 3;
     }
 
@@ -43,22 +47,20 @@ public class Team19FlushTest {
         iim.addDocument(new Document("wolf dog"));
         iim.flush();
         assertEquals(2, iim.getNumSegments());
-
         Map<String, List<Integer>> PostingList = new HashMap<>();
-        List<Integer> l = new LinkedList<Integer>();
-        l.add(0);
-        l.add(1);
-        PostingList.put("cat", l);
-        List<Integer> l2 = new LinkedList<Integer>();
+        List<Integer> l1 = new LinkedList<>();
+        l1.add(0);
+        l1.add(1);
+        PostingList.put("cat", l1);
+        List<Integer> l2 = new LinkedList<>();
         l2.add(0);
         PostingList.put("dog", l2);
-        List<Integer> l3 = new LinkedList<Integer>();
+        List<Integer> l3 = new LinkedList<>();
         l3.add(1);
-        PostingList.put("elephant", l);
+        PostingList.put("elephant", l3);
         Map<Integer, Document> DocStore = new HashMap<>();
         DocStore.put(0, new Document("cat dog"));
         DocStore.put(1, new Document("cat elephant"));
-
         InvertedIndexSegmentForTest test = iim.getIndexSegment(0);
         assertEquals(PostingList, test.getInvertedLists());
         assertEquals(DocStore, test.getDocuments());
@@ -72,7 +74,6 @@ public class Team19FlushTest {
         Document d2 = new Document("Searches can be based on full-text or other content-based indexing");
         Document d3 = new Document("The process may then be iterated if the user wishes to refine the query");
         Document d4 = new Document("Web search engines are the most visible IR applications");
-
         iim.addDocument(d1);
         iim.addDocument(d2);
         iim.addDocument(d3);
@@ -80,18 +81,72 @@ public class Team19FlushTest {
         iim.addDocument(d4);
         iim.flush();
         assertEquals(2, iim.getNumSegments());
-
         Set<Document> result = new HashSet<>();
         Set<Document> expected = new HashSet<>();
         expected.add(d1);
         expected.add(d2);
         expected.add(d3);
         expected.add(d4);
-
         while(iim.documentIterator().hasNext()) {
             result.add(iim.documentIterator().next());
         }
+        assertEquals(result, expected);
+    }
 
+    // test when flush() is called, if the second segment have a correct inverted index
+    // and if the total number of segments and documentIterator match
+    @Test
+    public void testFlush3() throws Exception {
+        String s1 = "fox lion tiger puma panther";
+        String s2 = "lion dog eagle giraffe fox";
+        String s3 = "budget director film movie actor";
+        String s4 = "theater actor director budget popcorn";
+        Map<String, List<Integer>> postingList = new HashMap<>();
+        List<Integer> l = new LinkedList<>();
+        l.add(0);
+        l.add(1);
+        postingList.put("actor", l);
+        postingList.put("budget", l);
+        postingList.put("director", l);
+        l = new LinkedList<>();
+        l.add(0);
+        postingList.put("film", l);
+        postingList.put("movie", l);
+        l = new LinkedList<>();
+        l.add(1);
+        postingList.put("popcorn", l);
+        postingList.put("theater", l);
+        String s5 = "pizza burger pasta salad sandwich";
+        String s6 = "salad sandwich noodle rice pasta";
+        Document d1 = new Document(s1);
+        Document d2 = new Document(s2);
+        Document d3 = new Document(s3);
+        Document d4 = new Document(s4);
+        Document d5 = new Document(s5);
+        Document d6 = new Document(s6);
+        iim.addDocument(d1);
+        iim.addDocument(d2);
+        iim.flush();
+        iim.addDocument(d3);
+        iim.addDocument(d4);
+        iim.flush();
+        iim.addDocument(d5);
+        iim.addDocument(d6);
+        iim.flush();
+        assertEquals(3, iim.getNumSegments());
+        InvertedIndexSegmentForTest num = iim.getIndexSegment(0);
+        assertEquals(postingList, num.getInvertedLists());
+        Set<Document> result = new HashSet<>();
+        Set<Document> expected = new HashSet<>();
+        expected.add(d1);
+        expected.add(d2);
+        expected.add(d3);
+        expected.add(d4);
+        expected.add(d5);
+        expected.add(d6);
+        while(iim.documentIterator().hasNext()) {
+            result.add(iim.documentIterator().next());
+        }
         assertEquals(result, expected);
     }
 
