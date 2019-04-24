29d0ebbbf0bc73726a1bf970dd9c6a2101077312
mengqiwangwmq <mengqw3@uci.edu>
2019-04-23 22:06:54 -0700

    commit

--- src/main/java/edu/uci/ics/cs221/index/inverted/InvertedIndexManager.java
+++ src/main/java/edu/uci/ics/cs221/index/inverted/InvertedIndexManager.java
@@ -1,26 +1,20 @@
 package edu.uci.ics.cs221.index.inverted;
-
 import com.google.common.base.Preconditions;
 import edu.uci.ics.cs221.analysis.Analyzer;
 import edu.uci.ics.cs221.storage.Document;
-
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-
 /**
  * This class manages an disk-based inverted index and all the documents in the inverted index.
  *
  * Please refer to the project 2 wiki page for implementation guidelines.
  */
 public class InvertedIndexManager {
-
     /**
      * The default flush threshold, in terms of number of documents.
      * For example, a new Segment should be automatically created whenever there's 1000 documents in the buffer.
@@ -28,7 +22,6 @@ public class InvertedIndexManager {
      * In test cases, the default flush threshold could possibly be set to any number.
      */
     public static int DEFAULT_FLUSH_THRESHOLD = 1000;
-
     /**
      * The default merge threshold, in terms of number of segments in the inverted index.
      * When the number of segments reaches the threshold, a merge should be automatically triggered.
@@ -36,13 +29,8 @@ public class InvertedIndexManager {
      * In test cases, the default merge threshold could possibly be set to any number.
      */
     public static int DEFAULT_MERGE_THRESHOLD = 8;
-
-
-    int numSegments = 0;
-
     private InvertedIndexManager(String indexFolder, Analyzer analyzer) {
     }
-
     /**
      * Creates an inverted index manager with the folder and an analyzer
      */
@@ -63,29 +51,29 @@ public class InvertedIndexManager {
             throw new UncheckedIOException(e);
         }
     }
-
     /**
      * Adds a document to the inverted index.
      * Document should live in a in-memory buffer until `flush()` is called to write the segment to disk.
      * @param document
      */
-    public void addDocument(Document document) { throw new UnsupportedOperationException();}
-
+    public void addDocument(Document document) {
+        throw new UnsupportedOperationException();
+    }
     /**
      * Flushes all the documents in the in-memory segment buffer to disk. If the buffer is empty, it should not do anything.
      * flush() writes the segment to disk containing the posting list and the corresponding document store.
      */
-    public void flush() { throw new UnsupportedOperationException();}
-
+    public void flush() {
+        throw new UnsupportedOperationException();
+    }
     /**
      * Merges all the disk segments of the inverted index pair-wise.
      */
     public void mergeAllSegments() {
-        // merge only happens at even number of segments
+// merge only happens at even number of segments
         Preconditions.checkArgument(getNumSegments() % 2 == 0);
         throw new UnsupportedOperationException();
     }
-
     /**
      * Performs a single keyword search on the inverted index.
      * You could assume the analyzer won't convert the keyword into multiple tokens.
@@ -96,10 +84,8 @@ public class InvertedIndexManager {
      */
     public Iterator<Document> searchQuery(String keyword) {
         Preconditions.checkNotNull(keyword);
-
         throw new UnsupportedOperationException();
     }
-
     /**
      * Performs an AND boolean search on the inverted index.
      *
@@ -108,10 +94,8 @@ public class InvertedIndexManager {
      */
     public Iterator<Document> searchAndQuery(List<String> keywords) {
         Preconditions.checkNotNull(keywords);
-
         throw new UnsupportedOperationException();
     }
-
     /**
      * Performs an OR boolean search on the inverted index.
      *
@@ -120,17 +104,14 @@ public class InvertedIndexManager {
      */
     public Iterator<Document> searchOrQuery(List<String> keywords) {
         Preconditions.checkNotNull(keywords);
-
         throw new UnsupportedOperationException();
     }
-
     /**
      * Iterates through all the documents in all disk segments.
      */
     public Iterator<Document> documentIterator() {
         throw new UnsupportedOperationException();
     }
-
     /**
      * Deletes all documents in all disk segments of the inverted index that match the query.
      * @param keyword
@@ -138,15 +119,15 @@ public class InvertedIndexManager {
     public void deleteDocuments(String keyword) {
         throw new UnsupportedOperationException();
     }
-
     /**
      * Gets the total number of segments in the inverted index.
      * This function is used for checking correctness in test cases.
      *
      * @return number of index segments.
      */
-    public int getNumSegments() { throw new UnsupportedOperationException();}
-
+    public int getNumSegments() {
+        throw new UnsupportedOperationException();
+    }
     /**
      * Reads a disk segment into memory based on segmentNum.
      * This function is mainly used for checking correctness in test cases.
@@ -154,7 +135,7 @@ public class InvertedIndexManager {
      * @param segmentNum n-th segment in the inverted index (start from 0).
      * @return in-memory data structure with all contents in the index segment, null if segmentNum don't exist.
      */
-    public InvertedIndexSegmentForTest getIndexSegment(int segmentNum) { throw new UnsupportedOperationException();}
-
-
+    public InvertedIndexSegmentForTest getIndexSegment(int segmentNum) {
+        throw new UnsupportedOperationException();
+    }
 }
