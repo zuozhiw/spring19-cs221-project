c86ff755f7bdf96a794b03bb337f727e1de81eb9
zerowei <43306378+zerowei@users.noreply.github.com>
2019-04-23 23:24:26 -0700

    Update Team11MergeSearchTest.java

--- src/test/java/edu/uci/ics/cs221/index/Team11MergeSearchTest.java
+++ src/test/java/edu/uci/ics/cs221/index/Team11MergeSearchTest.java
@@ -5,12 +5,17 @@ import edu.uci.ics.cs221.analysis.ComposableAnalyzer;
 import edu.uci.ics.cs221.analysis.PorterStemmer;
 import edu.uci.ics.cs221.analysis.PunctuationTokenizer;
 import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
+import edu.uci.ics.cs221.index.inverted.InvertedIndexSegmentForTest;
 import edu.uci.ics.cs221.storage.Document;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.List;
 import java.io.File;
 
 public class Team11MergeSearchTest {
@@ -52,7 +57,7 @@ public class Team11MergeSearchTest {
             System.out.println("Something went wrong when deleting file");
         }
     }
-
+    
     @Test
     public void mergeSearchTest1() {
         InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = 1;
@@ -64,6 +69,10 @@ public class Team11MergeSearchTest {
         }
     }
 
+    /* Test an easy case of flush threshold=1 and merge threshold=2,
+     * after we add 2 documents, the number of segments after merging
+     * should be 1
+     */
     @Test
     public void mergeSearchTest2() {
         InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = 1;
@@ -75,6 +84,9 @@ public class Team11MergeSearchTest {
         assertEquals(expectedNumSegments, index.getNumSegments());
     }
 
+    /* Test when the number of segments is 3 and we forcefully use
+     * mergeAllSegments() and then the result of merging should still be 3
+     */
     @Test
     public void mergeSearchTest3() {
         InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = 2;
@@ -85,5 +97,28 @@ public class Team11MergeSearchTest {
         index.mergeAllSegments();
         int expectedNumSegments = 3;
         assertEquals(expectedNumSegments, index.getNumSegments());
+    }
+    /* Test when the flush and merge thresholds are 1 and 4 respectively,
+     * after merging the number of segments should be 3 and we also check
+     * the detailed docIDs of the keywords
+     */
+    @Test
+    public void mergeSearchTest4() {
+        InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = 1;
+        InvertedIndexManager.DEFAULT_MERGE_THRESHOLD = 4;
+
+        for (Document doc : documents) {
+            index.addDocument(doc);
+        }
+        int expectedNumSegments = 3;
+        assertEquals(expectedNumSegments, index.getNumSegments());
+        InvertedIndexSegmentForTest it = index.getIndexSegment(0);
+        Map<String, List<Integer>> invertedLists = it.getInvertedLists();
+        List<Integer> docIds = invertedLists.get("import");
+        List<Integer> expectedDocIds = Arrays.asList(0, 1, 2, 3);
+        assertEquals(expectedDocIds, docIds);
+        List<Integer> docIds1 = invertedLists.get("composableanalyz");
+        List<Integer> expectedDocIds1 = Arrays.asList(1);
+        assertEquals(expectedDocIds1, docIds1);
     }
 }
