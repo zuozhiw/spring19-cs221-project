d77584b932a865cd43386f19e0122af16ad4b127
Shao, Zhetai <zhetais@uci.edu>
2019-04-24 17:35:21 -0700

    add counter

--- src/test/java/edu/uci/ics/cs221/index/inverted/Team3StressTest.java
+++ src/test/java/edu/uci/ics/cs221/index/inverted/Team3StressTest.java
@@ -15,93 +15,115 @@ import static junit.framework.TestCase.assertTrue;
 
 public class Team3StressTest {
 
-    Analyzer analyzer1;
+    Analyzer analyzer;
     List<String> allDocuments;
-    InvertedIndexManager invertedIndexManager1;
-    static final int TOTALNUM = 100000;
+    InvertedIndexManager invertedIndexManager;
+    private static final int TOTALNUM = 100000;
+    private static final String textUrl = "http://cyy0908.com/text.txt";
+    private static final String indexFolder = "./index/Team3StressTest/";
+
 
     //Construct to do some initialization like downloading the text resources
     //and objects construction.
     public Team3StressTest(){
-        String URL = "http://cyy0908.com/text.txt";
-        allDocuments = new ArrayList<>();
-        try {
-            java.net.URL url = new URL(URL);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            String s;
-            while ((s = reader.readLine()) != null) {
-                allDocuments.add(s);
-            }
-            reader.close();
-        }
-        catch (Exception e){
-            new RuntimeException(e);
-        }
-
-        analyzer1 = new ComposableAnalyzer(new PunctuationTokenizer(),new PorterStemmer());
-        invertedIndexManager1 = InvertedIndexManager.createOrOpen("./index/Team3StressTest/", analyzer1);
-
+        allDocuments = getOnlineTextFile(textUrl);
+        analyzer = new ComposableAnalyzer(new PunctuationTokenizer(),new PorterStemmer());
+        invertedIndexManager = InvertedIndexManager.createOrOpen(indexFolder, analyzer);
+        PageFileChannel.resetCounters();
         //Copy the resource text 100000 times so that the total resources will be very large
         //so that we could meet the demand of a Stress Test.(Very large dataset)
         for(int i=0;i<TOTALNUM;i++){
-            invertedIndexManager1.addDocument(new Document(allDocuments.get(i%allDocuments.size())));
+            invertedIndexManager.addDocument(new Document(allDocuments.get(i%allDocuments.size())));
         }
-
+        assertTrue(PageFileChannel.writeCounter>=TOTALNUM/invertedIndexManager.DEFAULT_FLUSH_THRESHOLD);
     }
+
+
     //Test searchQuery with keyword "CD" and see if it finds more than 500 results.
     //If true it means the engine didn't crash under a Stress Condition.
     @Test
     public void test1(){
         PageFileChannel.resetCounters();
-        Iterator<Document> result1 = invertedIndexManager1.searchQuery("CD");
+        Iterator<Document> result1 = invertedIndexManager.searchQuery("CD");
         int count = 0;
         while(result1.hasNext()){
             result1.next();
             count++;
         }
-        assertTrue(PageFileChannel.writeCounter>=500);
-        assertTrue(PageFileChannel.readCounter>=500);
-        assertTrue(count>500?true:false);
+        assertTrue(PageFileChannel.readCounter>=50);
+        assertTrue(count>=500);
     }
+
 
     //Test searchQuery with keyword "DVD" and see if it finds more than 1000 results.
     //If true it means the engine didn't crash under a Stress Condition.
     @Test
     public void test2(){
         PageFileChannel.resetCounters();
-        Iterator<Document> result1 = invertedIndexManager1.searchQuery("DVD");
+        Iterator<Document> result1 = invertedIndexManager.searchQuery("DVD");
         int count = 0;
         while(result1.hasNext()){
             result1.next();
             count++;
         }
-        assertTrue(PageFileChannel.writeCounter>=500);
-        assertTrue(PageFileChannel.readCounter>=500);
-        assertTrue(count>1000?true:false);
+        assertTrue(PageFileChannel.readCounter>=100);
+        assertTrue(count>=1000);
     }
+
+
     //Test searchAndQuery with keyword "CD" and "DVD" and see if it finds no answer.
     @Test
     public void test3(){
-        Iterator<Document> result1 = invertedIndexManager1.searchAndQuery(Arrays.asList("DVD","CD"));
+        Iterator<Document> result1 = invertedIndexManager.searchAndQuery(Arrays.asList("DVD","CD"));
         assertTrue(!result1.hasNext());
     }
+
+
     //Test searchOrQuery with keyword "DVD" or "CD" and see if it finds more than 1500 results.
     //If true it means the engine didn't crash under a Stress Condition.
     @Test
     public void test4(){
-        Iterator<Document> result1 = invertedIndexManager1.searchOrQuery(Arrays.asList("DVD","CD"));
+        PageFileChannel.resetCounters();
+        Iterator<Document> result1 = invertedIndexManager.searchOrQuery(Arrays.asList("DVD","CD"));
         int count = 0;
         while(result1.hasNext()){
             result1.next();
             count++;
         }
-        assertTrue(count>1500?true:false);
+        assertTrue(count>=1500);
+        assertTrue(PageFileChannel.readCounter>=150);
     }
+
+
     //Delete the files that are created during the process of searching.
     @After
     public void after(){
-        String path = "./index/Team3StressTest/";
-        File file = new File(path);
+        deleteFile(indexFolder);
+    }
+
+
+    //Get our large resources text file from a URL
+    private List<String> getOnlineTextFile(String URL){
+        List<String> result = new ArrayList<>();
+        try {
+            java.net.URL url = new URL(URL);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String s;
+            while ((s = reader.readLine()) != null) {
+                result.add(s);
+            }
+            reader.close();
+        }
+        catch (Exception e){
+            new RuntimeException(e);
+        }
+        return result;
+    }
+
+
+    //Delete the index/Team directory
+    private void deleteFile(String fileDir){
+        File file = new File(fileDir);
         if(!file.isDirectory()){
             System.out.println("File name is not a directory");
             return;
@@ -109,15 +131,14 @@ public class Team3StressTest {
         String[] tempList = file.list();
         File temp = null;
         for (int i = 0; i < tempList.length; i++) {
-            if (path.endsWith(File.separator)) {
-                temp = new File(path + tempList[i]);
+            if (fileDir.endsWith(File.separator)) {
+                temp = new File(fileDir + tempList[i]);
             } else {
-                temp = new File(path + File.separator + tempList[i]);
+                temp = new File(fileDir + File.separator + tempList[i]);
             }
             if (temp.isFile()) {
                 temp.delete();
             }
         }
-
     }
 }
