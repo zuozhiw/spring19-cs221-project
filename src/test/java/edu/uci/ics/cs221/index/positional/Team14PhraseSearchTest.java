f87d6abc3ff08dba68da34734b9e2250ae915eb8
omidkv <omidkv@gmail.com>
2019-05-08 17:56:56 -0700

    started team14PhraseTest

--- src/test/java/edu/uci/ics/cs221/index/positional/Team14PhraseSearchTest.java
+++ src/test/java/edu/uci/ics/cs221/index/positional/Team14PhraseSearchTest.java
@@ -1,4 +1,46 @@
 package edu.uci.ics.cs221.index.positional;
 
+import edu.uci.ics.cs221.analysis.Analyzer;
+import edu.uci.ics.cs221.analysis.ComposableAnalyzer;
+import edu.uci.ics.cs221.analysis.PorterStemmer;
+import edu.uci.ics.cs221.analysis.PunctuationTokenizer;
+import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
+import edu.uci.ics.cs221.index.inverted.InvertedIndexSegmentForTest;
+import edu.uci.ics.cs221.storage.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.print.Doc;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+
 public class Team14PhraseSearchTest {
+
+    InvertedIndexManager index;
+    Analyzer analyzer = new ComposableAnalyzer(new PunctuationTokenizer(), new PorterStemmer());
+    String path = "./index/Team14MergeTest/";
+
+
+    @Before public void build() {
+        index = InvertedIndexManager.createOrOpen(path, analyzer);
+        InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = 1;
+    }
+
+    @After public void tear() {
+        File index = new File(path);
+        String[] entries = index.list();
+        for (String s : entries) {
+            File currentFile = new File(index.getPath(), s);
+            currentFile.delete();
+        }
+        index.delete();
+        InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = 1000;
+        InvertedIndexManager.DEFAULT_MERGE_THRESHOLD = 8;
+    }
 }
