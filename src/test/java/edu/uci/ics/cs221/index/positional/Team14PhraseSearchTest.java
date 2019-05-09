6f5cf28b0725e58256f933b3f6e89d46952908c8
mikhail <mikhail.lychagin@gmail.com>
2019-05-08 19:05:38 -0700

    Finished Test Case 1

--- src/test/java/edu/uci/ics/cs221/index/positional/Team14PhraseSearchTest.java
+++ src/test/java/edu/uci/ics/cs221/index/positional/Team14PhraseSearchTest.java
@@ -7,18 +7,17 @@ import edu.uci.ics.cs221.analysis.PunctuationTokenizer;
 import edu.uci.ics.cs221.index.inverted.Compressor;
 import edu.uci.ics.cs221.index.inverted.DeltaVarLenCompressor;
 import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
-import edu.uci.ics.cs221.index.inverted.InvertedIndexSegmentForTest;
 import edu.uci.ics.cs221.storage.Document;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.print.Doc;
 import java.io.File;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
 
 public class Team14PhraseSearchTest {
     InvertedIndexManager index;
@@ -26,20 +25,16 @@ public class Team14PhraseSearchTest {
     Compressor compressor = new DeltaVarLenCompressor();
     String path = "./index/Team14PhraseSearchTest/";
 
-
-
-    Document[] documents1 = new Document[] {
-            new Document("This morning I ate eggs"),
+    Document[] documents1 = new Document[] { new Document("This morning I ate eggs"),
             new Document("Abstraction is often one floor above you."),
             new Document("Everyone was busy, so I went to the movie alone."),
             new Document("Please wait outside of the house."),
             new Document("Wednesday is hump day, but has anyone asked the camel if he’s happy about it?"),
             new Document("He told us a very exciting adventure story."),
             new Document("My Mom tries to be cool by saying that she likes all the same things that I do."),
-            new Document("She advised him to come back at once."),
-            new Document("She works two jobs to make ends meet; at least, that was her reason for not having time to join us."),
-            new Document("How was the math test?"),
-            new Document("Eggs come from chickens."),
+            new Document("She advised him to come back at once."), new Document(
+            "She works two jobs to make ends meet; at least, that was her reason for not having time to join us."),
+            new Document("How was the math test?"), new Document("Eggs come from chickens."),
             new Document("Abstraction is used in this project."),
             new Document("Everyone was busy with math homework, and so I went out alone"),
             new Document("The job we did alone at the house of my boss and we weren't happy about it."),
@@ -54,6 +49,25 @@ public class Team14PhraseSearchTest {
             new Document("Two jobs to make ends meet, means we need to less eggs."),
             new Document("As a camel do you have one or two humps?") };
 
+    Document[] documents2 = new Document[] { new Document("Hello"), new Document("I like to eat pineapples."),
+            new Document("Last week I took the express train to San Diego."),
+            new Document("Pineapple Express was a great movie."),
+            new Document("Mother always said to eat my vegetables, but I never listened."),
+            new Document("Fridays are the best part of my week."), new Document("Last Friday I watched a movie."),
+            new Document("Next Friday I will watch the new Avengers movie."),
+            new Document("I've started a new diet with vegetables and I've had a terrible week."),
+            new Document("Atleast I can still eat pineapples."), new Document("My mother would be proud of me."),
+            new Document("I ate a lot of pineapples in San Diego."),
+            new Document("I can't believe mother keeps eating all of my chocolate."),
+            new Document("I live for chocolate and pineapples"),
+            new Document("My favorite activity is watching a movie and eating pineapples at the same time"),
+            new Document("Last Friday I ate my pineapples diced"),
+            new Document("Next week I will try eating my pineapple raw"),
+            new Document("I wonder if next week I will take the express back to San Diego"),
+            new Document("Don't tell mother but I stole her credit card and used it to buy pineapples"),
+            new Document("I predict the new Avengers movie will be worthy of a diced pineapples"),
+            new Document("Unfortudently, the movie theater doesn't sell diced pineapples"),
+            new Document("I'm going to have to find a way to get my diced pineapples into the movie theater") };
 
     @Before public void build() {
         index = InvertedIndexManager.createOrOpenPositional(path, analyzer, compressor);
@@ -72,33 +86,36 @@ public class Team14PhraseSearchTest {
         InvertedIndexManager.DEFAULT_MERGE_THRESHOLD = 8;
     }
 
-    @Test
-    public void test1() {
+    @Test public void test1() {
         InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = 1000;
+        for (Document doc : documents2) {
+            index.addDocument(doc);
+        }
+        queryIndex(Arrays.asList("movie", "theater"), 2);
+        queryIndex(Arrays.asList("credit", "card"), 1);
+        queryIndex(Arrays.asList("diced", "pineapples"), 3);
+        queryIndex(Arrays.asList("eat", "pineapples"), 2);
+        queryIndex(Arrays.asList("San", "Diego"), 3);
+        queryIndex(Arrays.asList("Avengers", "Movie"), 2);
+        queryIndex(Arrays.asList("next", "week"), 2);
+        queryIndex(Arrays.asList("Last", "Friday"), 2);
     }
 
-
-    @Test public void test3()
-    {
-
-        for(Document doc : documents1)
-        {
+    @Test public void test3() {
+        for (Document doc : documents1) {
             index.addDocument(doc);
-
         }
         index.flush();
+        queryIndex(Arrays.asList("never", "mind"), 0);
+    }
 
-        List<String> keyWords = Arrays.asList("never", "mind");
+    private void queryIndex(List<String> keyWords, int expectedCount) {
         Iterator<Document> it = index.searchPhraseQuery(keyWords);
-
         int counter = 0;
-        while(it.hasNext())
-        {
-            counter ++;
+        while (it.hasNext()) {
+            counter++;
             it.next();
         }
-
-        assertEquals(0,counter);
-
+        assertEquals(expectedCount, counter);
     }
 }
