1405c2b3e73d0b551ccc316ace81c67a60ea4b8a
Kaifu J <jkf0128@gmail.com>
2019-05-10 19:56:02 -0700

    finished testcase

--- src/test/java/edu/uci/ics/cs221/index/positional/Team10PositionalStressTest.java
+++ src/test/java/edu/uci/ics/cs221/index/positional/Team10PositionalStressTest.java
@@ -20,68 +20,86 @@ public class Team10PositionalStressTest {
 
     private String path = "./index/Team10PositionalStressTest";
     private String bookurl = "http://www.gutenberg.org/cache/epub/4276/pg4276.txt";
-    private InvertedIndexManager invertedmanager;
+    private InvertedIndexManager iim;
     private List<String> docs;
 
-    @Test(timeout = 1500000)
+    @Test(timeout = 600000)
     public void init(){
-        invertedmanager = InvertedIndexManager.createOrOpenPositional(path,new ComposableAnalyzer(new PunctuationTokenizer(),new PorterStemmer()),new NaiveCompressor());
-        invertedmanager.DEFAULT_FLUSH_THRESHOLD = 1000;
-        invertedmanager.DEFAULT_MERGE_THRESHOLD = 10;
+        iim = InvertedIndexManager.createOrOpenPositional(path, 
+                                new ComposableAnalyzer(new PunctuationTokenizer(), new PorterStemmer()), 
+                                new DeltaVarLenCompressor());
+        
+        int oldFlushThreshold = iim.DEFAULT_FLUSH_THRESHOLD;
+        int oldMergeThreshold = iim.DEFAULT_MERGE_THRESHOLD;
+
+        iim.DEFAULT_FLUSH_THRESHOLD = 500;
+        iim.DEFAULT_MERGE_THRESHOLD = 20;
         docs = new ArrayList<>();
-        try {
-            java.net.URL url = new URL(bookurl);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                docs.add(line);
-            }
-            bufferedReader.close();
-        }catch(Exception e){
-            throw new RuntimeException(e);
+
+        java.net.URL url = new URL(bookurl);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            docs.add(line);
         }
+        bufferedReader.close();
+
         for(int i = 0; i < docs.size(); i++){
-            invertedmanager.addDocument(new Document(docs.get(i)));
+            iim.addDocument(new Document(docs.get(i)));
         }
+        iim.addDocument(new Document("zhege testcase wojuede youdianfan"));
+        iim.addDocument(new Document("wohaiyao quebao zhege document limian meiyou stopwords"));
+        iim.flush();
 
-        try{
-            test1();
-        }catch (Throwable e){
-            System.out.println("Sorry, u failed at test1");
-            e.printStackTrace();
-        }
+        test1();
+        test2();
+        test3();
+        test4();
 
-        try{
-            test2();
-        }catch (Throwable e){
-            System.out.println("Sorry, u failed at test2");
-            e.printStackTrace();
-        }
-
-        try{
-            test3();
-        }catch (Throwable e){
-            System.out.println("Sorry, u failed at test3");
-            e.printStackTrace();
-        }
+        iim.DEFAULT_FLUSH_THRESHOLD = oldFlushThreshold;
+        iim.DEFAULT_MERGE_THRESHOLD = oldMergeThreshold;
     }
 
+    // make sure searchQuery give right number of results
     public void test1(){
-        Iterator<Document> result = invertedmanager.searchQuery("beebo");
-        assertEquals(false, result.hasNext());
+        Iterator<Document> result = iim.searchQuery("ebook");
+        int count = 0;
+        while(result.hasNext()){
+            count++;
+            result.next();
+        }
+        assertEquals(17, count);
     }
 
+    // check if Phrase Query work properly
     public void test2(){
-        PositionalIndexSegmentForTest result = invertedmanager.getIndexSegmentPositional(0);
-        Map<Integer, Document> documents = result.getDocuments();
-        assertEquals("The Project Gutenberg EBook of North and South, by Elizabeth Cleghorn Gaskell",documents.get(0).getText());
+        Iterator<Document> res = iim.searchPhraseQuery(new ArrayList<String>(Arrays.asList("wojuede", "youdianfan")));
+
+        Document doc = res.next();
+
+        assertEquals("zhege testcase wojuede youdianfan", doc.getText());
     }
 
+    // check if position index works properly
+    // check if stopwords are ignored when calculate position
     public void test3(){
-        PositionalIndexSegmentForTest result = invertedmanager.getIndexSegmentPositional(0);
+        PositionalIndexSegmentForTest result = iim.getIndexSegmentPositional(0);
         Table<String, Integer, List<Integer>> position = result.getPositions();
-        assertEquals(new ArrayList<>(Arrays.asList(3)),position.get("ebook",0));
 
+        // We prefer 0-based position.
+        // If your implementation is 1-based, you can change the number to '3'
+        assertEquals(new ArrayList<Integer>(Arrays.asList(2)), position.get("ebook", 0));
+    }
+
+    // check if position index works properly
+    public void test4(){
+        int segNum = iim.getNumSegments();
+
+        PositionalIndexSegmentForTest res = iim.getIndexSegmentPositional(segNum - 1);
+        Table<String, Integer, List<Integer>> position = res.getPositions();
+        ArrayList<Integer> positionList = position.get("meiyou", iim.getDocuments().size() - 1);
+
+        assertEquals(new ArrayList<Integer>(Arrays.asList(5)), positionList);
     }
 
     @After
