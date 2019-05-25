4fee50d6a377d810ea96c8a5fbaacfd7e4b60355
zuozhiw <zuozhiw@gmail.com>
2019-05-25 00:44:55 -0700

    add p4 task 1 api

--- src/main/java/edu/uci/ics/cs221/index/inverted/InvertedIndexManager.java
+++ src/main/java/edu/uci/ics/cs221/index/inverted/InvertedIndexManager.java
@@ -151,6 +151,37 @@ public class InvertedIndexManager {
     }
 
     /**
+     * Performs top-K ranked search using TF-IDF.
+     * Returns an iterator that returns the K documents with highest TF-IDF scores.
+     *
+     * Unlike Boolean Query and Phrase Query where order of the documents doesn't matter,
+     * for ranked search, order of the document returned by the iterator matters.
+     *
+     * @param keywords, a list of keywords in the query
+     * @param topK, number of top documents weighted by TF-IDF
+     * @return a iterator of ordered documents matching the query
+     */
+    public Iterator<Document> searchTfIdf(List<String> keywords, int topK) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns the total number of documents within the given segment.
+     */
+    public int getNumDocuments(int segmentNum) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns the number of documents containing the token within the given segment.
+     * The token should be already analyzed by the analyzer. The analyzer shouldn't be applied again.
+     */
+    public int getDocumentFrequency(int segmentNum, String token) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    /**
      * Iterates through all the documents in all disk segments.
      */
     public Iterator<Document> documentIterator() {
