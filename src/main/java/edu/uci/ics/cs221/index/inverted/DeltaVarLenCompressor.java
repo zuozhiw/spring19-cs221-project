229e657442b16a0aecd218324bf93be7fa8a6e63
cyy12345 <892471766@qq.com>
2019-05-26 22:43:51 -0700

    cyy fix git

--- src/main/java/edu/uci/ics/cs221/index/inverted/DeltaVarLenCompressor.java
+++ src/main/java/edu/uci/ics/cs221/index/inverted/DeltaVarLenCompressor.java
@@ -1,6 +1,5 @@
 package edu.uci.ics.cs221.index.inverted;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,56 +10,11 @@ public class DeltaVarLenCompressor implements Compressor {
 
     @Override
     public byte[] encode(List<Integer> integers) {
-
-        List<Integer> deltaEncoded = new ArrayList<>();
-        deltaEncoded.add(integers.get(0));
-        for(int i=0;i<integers.size()-1;i++){
-            deltaEncoded.add(integers.get(i+1)-integers.get(i));
-        }
-        List<Byte> result = new ArrayList<>();
-        for(int i=0;i<deltaEncoded.size();i++){
-            List<Byte> curNumByte = new ArrayList<>();
-            encodeInteger(deltaEncoded.get(i),curNumByte);
-            for(int j=curNumByte.size()-1;j>=0;j--){
-                result.add(curNumByte.get(j));
-            }
-        }
-        byte[] encodedByteArray = new byte[result.size()];
-        for(int i=0;i<result.size();i++) encodedByteArray[i] = result.get(i);
-        return encodedByteArray;
-        //throw new UnsupportedOperationException();
-    }
-
-    private void encodeInteger(int a, List<Byte> encodedList){
-        int mod = a%128;
-        encodedList.add((byte)mod);
-        int res = a/128;
-        while(res>=1){
-            int resMod = res%128+128;
-            encodedList.add((byte)resMod);
-            res /= 128;
-        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public List<Integer> decode(byte[] bytes, int start, int length) {
-        //throw new UnsupportedOperationException();
-        List<Integer> result = new ArrayList<>();
-        int totalLength = start+length;
-        int i = start;
-        int offset = 0;
-        while(i<totalLength) {
-            int num = 0;
-            while (bytes[i] < 0) {
-                int cur = num + 128 + bytes[i];
-                num = cur * 128;
-                i++;
-            }
-            num += bytes[i];
-            offset += num;
-            result.add(offset);
-            i++;
-        }
-        return result;
+        throw new UnsupportedOperationException();
     }
 }
