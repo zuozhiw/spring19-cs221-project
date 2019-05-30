d9f26c7e64d0ecceabed4d3013d64328ad202cdb
KaifuJ <jkf0128@gmail.com>
2019-05-29 21:19:04 -0700

    finished proj4 test case.

--- src/test/java/edu/uci/ics/cs221/index/ranking/Team10searchTfIdfTest.java
+++ src/test/java/edu/uci/ics/cs221/index/ranking/Team10searchTfIdfTest.java
@@ -7,12 +7,15 @@ import edu.uci.ics.cs221.index.inverted.InvertedIndexSegmentForTest;
 import edu.uci.ics.cs221.storage.Document;
 import org.junit.After;
 import org.junit.Test;
+import edu.uci.ics.cs221.index.inverted.Pair;
 
 import java.io.File;
 
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class Team10searchTfIdfTest {
     private String path = "./index/Team10searchTfIdfTest";
@@ -26,19 +29,29 @@ public class Team10searchTfIdfTest {
         Document doc1 = new Document("test search project");
         Document doc2 = new Document("search engine is our project");
         Document doc3 = new Document("we need to finish the project");
+
         iim.addDocument(doc1);
-        iim.addDocument(doc2);
         iim.addDocument(doc2);
         iim.addDocument(doc3);
         iim.flush();
+
         List<String> phrase = new ArrayList<>(Arrays.asList("search","engine","project"));
-        Iterator<Document> res = iim.searchTfIdf(phrase,2);
-        int count = 0;
-        while(res.hasNext()){
-            count += 1;
-            assertEquals(res,doc2);
-        }
-        assertEquals(count,2);
+        Iterator<Pair<Document, Double>> res = iim.searchTfIdf(phrase,2);
+
+        Pair<Document, Double> res0 = res.next();
+        Pair<Document, Double> res1 = res.next();
+
+        // make sure we get the correct num of results
+        assertFalse(res.hasNext());
+
+        // check if we get the correct documents in the correct order
+        assertEquals(res0.getLeft(), doc2);
+        assertEquals(res1.getLeft(), doc1);
+
+        // check if the scores are calculated correctly
+        // similarity of doc2 and doc1 are 0.176 and 0.508 respectively
+        assertTrue(0.18 - res0.getRight() < 0.01);
+        assertTrue(0.51 - res1.getRight() < 0.01);
     }
 
     @After
