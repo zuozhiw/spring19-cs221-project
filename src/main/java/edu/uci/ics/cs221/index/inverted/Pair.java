a3611bcdabeea8fd868d6ac85dd164911946cc28
Pritha Dawn <prithadawn@Prithas-MacBook-Air.local>
2019-05-30 21:39:07 -0700

    test for P4

---
+++ src/main/java/edu/uci/ics/cs221/index/inverted/Pair.java
@@ -0,0 +1,48 @@
+package edu.uci.ics.cs221.index.inverted;
+
+import java.util.Objects;
+
+public class Pair<L, R> {
+
+    private final L left;
+    private final R right;
+
+    public Pair<L, R> of(L left, R right) {
+        return new Pair<>(left, right);
+    }
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(left, pair.left) &&
+                Objects.equals(right, pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
